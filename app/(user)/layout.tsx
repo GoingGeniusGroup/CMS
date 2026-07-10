621e4bc76641a92c3935f763cdb7dec516e76414
Arjun Gautam <gtmarjun10@gmail.com>
2026-07-10 15:15:10 +0545

    Connect General Settings with Client Website-3217

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -8,7 +8,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
   return {
     title: {
-      default: `${settings.siteName} — Group of Companies`,
+      default: settings.siteName,
       template: `%s | ${settings.siteName}`,
     },
     description: settings.description,
